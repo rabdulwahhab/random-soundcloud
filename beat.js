e63efea34e5033dfa0ea1a4956eaed29b0467c76
rayyan <rayyan10@bellsouth.net>
2020-09-04 13:37:19 -0500

    📦  NEW: Add jQuery

--- beat.js
+++ beat.js
@@ -6,9 +6,18 @@ const scdl = require('soundcloud-downloader');
 // the function inside 'createServer' gets executed whenever
 // a request is received on the specified port
 http.createServer(function (req, res) {
+  /*res.writeHead(200, {
+    'Content-Type': 'text/html'
+  });
+  res.write('hi again');*/
   fs.readFile('page.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
   });
+  //res.end();
 }).listen(80);
+
+function test() {
+  console.log("This works!");
+}
