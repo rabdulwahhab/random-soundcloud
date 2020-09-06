fd6b33313462ebe4cd8b1fd52dfc1f97105596cb
rayyan <rayyan10@bellsouth.net>
2020-09-06 18:32:33 -0500

    📦  NEW: added corny favicon ..

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,8 +3,6 @@ const scdl = require('soundcloud-downloader');
 const fs = require('fs');
 
 // TODO bring in processing logic here and pass output to views
-let i = -1;
-let carousel = [null, null, null, null, null, null, null, null, null, null];
 const MAX_DURATION = 6 // in minutes
 MAX_PLAYS = 100000;
 
@@ -27,33 +25,11 @@ function nextTrackURL(track_id) {
     return temp;
 }
 
-// Updates carousel to keep consistent state (cache + current track index)
-// Call when the next acceptable track is found.
-function next(trackObj) {
-    logger(i);
-    i = (i + 1) % 10;
-    carousel[i] = trackObj;
-    logger(carousel);
-}
-
-// Returns previous track JSON obj from cache and updates state (current track index)
-function back() {
-    i = i - 1;
-    if (i <= 0) i = 0;
-    logger(i);
-    logger(carousel);
-    return carousel[i % 10];
-}
-
-const test = (res) => {
-    logger("Passed a function!");
-    res.send('ok');
-}
-
 // This is for exporting components that can be used by node in app.js.
 // Passing in the express var
 module.exports = function (app) {
 
+    // ROUTES
     app.get('/', function (req, res) {
         const trackObj = {title: "Random Soundcloud Tracks", artist: "", url: ""};
         res.render('page', {track: trackObj});
