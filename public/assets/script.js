0da0d1cf985a3df966cacce32748ba6338b6cd10
rayyan <rayyan10@bellsouth.net>
2020-09-07 21:32:58 -0500

    ⚡  IMPROVE: Client side caching

--- public/assets/script.js
+++ public/assets/script.js
@@ -2,14 +2,16 @@
 
 let i = -1;
 let NUM_REQUESTS = 50;
-const CAROUSEL_SIZE = 30;
-let NUM_CACHED = 0;
-const carousel = [];
-for (let j = 0; j < CAROUSEL_SIZE; ++j) {
-    carousel.push(null);
-}
+//const CACHE_SIZE = 30;
+//let NUM_CACHED = 0;
+const CACHE = [];
+const HISTORY = [null, null, null, null, null];
+let CURRENT = null;
+//for (let j = 0; j < CACHE_SIZE; ++j) {
+//    cache.push(null);
+//}
 
-//let carousel = [null, null, null, null, null, null, null, null, null, null];
+//let cache = [null, null, null, null, null, null, null, null, null, null];
 
 function logger(msg) {
     console.log(msg);
@@ -18,7 +20,7 @@ function logger(msg) {
 // Updates carousel to keep consistent state (current track index)
 // NOTE: CALL ONLY WHEN VALID NEXT TRACK IS OBTAINED
 function next() {
-    i = (i + 1) % CAROUSEL_SIZE;
+    i = (i + 1) % CACHE_SIZE;
     logger("CURRENT i: " + i);
     //carousel[i] = trackObj;
     //logger(carousel);
@@ -28,8 +30,8 @@ function next() {
 function back() {
     // TODO modulo bug for negative numbers
     carousel[i] = null;
-    i = (((i - 1) % CAROUSEL_SIZE) + CAROUSEL_SIZE) % CAROUSEL_SIZE;
-    //i = (i - 1) % CAROUSEL_SIZE;
+    i = (((i - 1) % CACHE_SIZE) + CACHE_SIZE) % CACHE_SIZE;
+    //i = (i - 1) % CACHE_SIZE;
     logger("CURRENT: " + i);
     logger(carousel);
     // const prev = carousel[i];
@@ -63,7 +65,7 @@ $(document).ready(() => {
         // Page handling
 
         // Make request for more tracks
-        if (NUM_CACHED < Math.round(CAROUSEL_SIZE / 3)) {
+        if (CACHE.length <= 7) { // TODO define threshold
             logger("CLIENT --- MAKING REQUESTS");
             $("#next").prop('disabled', true);
             $("#loading_icon").fadeIn(2000);
@@ -74,52 +76,63 @@ $(document).ready(() => {
                 data: {numRequests: NUM_REQUESTS} // TODO implement sliding window
             }).done(function (data) {
                 logger("CLIENT --- RESPONSE RECEIVED");
-                $("#loading_icon").toggle();
                 let tracks = data.tracks;
                 logger("RECEIVED " + tracks.length + " TRACKS");
-                // TODO will return undef if no tracks found
-                next(); // update state
-                carousel[i] = tracks.shift(); // load a track
-                logger("AFTER POP, " + tracks.length + " RAW TRACKS:");
-                tracks.map(e => logger(e));
+                while (tracks.length > 0) {
+                    //logger(j);
+                    CACHE.push(tracks.shift());
+                    //logger("IN LOOP:");
+                    //NUM_CACHED = NUM_CACHED + 1;
+                    //logger(tracks.shift());
+                    //j = (j + 1) % CACHE_SIZE;
+                }
+
+                // TODO update DOM
+                // store in history
+                HISTORY.push(CURRENT);
+                HISTORY.shift();
+                CURRENT = CACHE.shift(); // TODO will return undef if no tracks found
+                logger("NEXT TRACK:");
+                logger(CURRENT);
+                logger(CACHE.length + " TRACKS IN CACHE");
+                logger("CACHE CONTAINS:");
+                logger(CACHE);
+                updateDom(CURRENT.title, CURRENT.url, CURRENT.artist);
+
+                $("#loading_icon").toggle();
+                $("#next").prop('disabled', false);
+                //
+                //next(); // update state
+                //carousel[i] = tracks.shift(); // load a track
+                //logger("AFTER POP, " + tracks.length + " RAW TRACKS:");
+                //tracks.map(e => logger(e));
 
                 // load cache with rest of tracks received
-                let j = (i + 1) % CAROUSEL_SIZE;
-                while (j !== i && tracks.length > 0) {
-                    //logger(j);
-                    carousel[j] = tracks.shift();
-                    //logger("IN LOOP:");
-                    NUM_CACHED = NUM_CACHED + 1;
-                    //logger(tracks.shift());
-                    j = (j + 1) % CAROUSEL_SIZE;
-                }
-                // TODO update DOM
-                const curr = carousel[i];
-                logger("NEXT TRACK:");
-                logger(curr);
-                logger(NUM_CACHED + " TRACKS IN CACHE");
-                logger("CAROUSEL NOW:");
-                logger(carousel);
-                updateDom(curr.title, curr.url, curr.artist);
-                $("#next").prop('disabled', false);
+                // let j = (i + 1) % CACHE_SIZE;
+                // while (j !== i && tracks.length > 0) {
+                //     //logger(j);
+                //     carousel[j] = tracks.shift();
+                //     //logger("IN LOOP:");
+                //     NUM_CACHED = NUM_CACHED + 1;
+                //     //logger(tracks.shift());
+                //     j = (j + 1) % CACHE_SIZE;
+                // }
             }).fail(function () {
                 alert("An error occurred. Please check your internet connection and try again.");
             });
         } else {
-            logger("CLIENT --- GET NEXT FROM CACHE");
-            // TODO get next from cache
-            next(); // advance state
-            while (!carousel[i]) {
-                next();
-            }
-            const curr = carousel[i];
+            logger("CLIENT --- GET NEXT TRACK FROM CACHE");
+            // TODO update DOM
+            // store in history
+            HISTORY.push(CURRENT);
+            HISTORY.shift();
+            CURRENT = CACHE.shift(); // TODO will return undef if no tracks found
             logger("NEXT TRACK:");
-            logger(curr);
-            NUM_CACHED = NUM_CACHED - 1;
-            logger(NUM_CACHED + " TRACKS LEFT IN CACHE");
-            logger("CAROUSEL NOW:");
-            logger(carousel);
-            updateDom(curr.title, curr.url, curr.artist);
+            logger(CURRENT);
+            logger(CACHE.length + " TRACKS IN CACHE");
+            logger("CACHE CONTAINS:");
+            logger(CACHE);
+            updateDom(CURRENT.title, CURRENT.url, CURRENT.artist)
             //$("#next").prop('disabled', false);
         }
     });
@@ -127,13 +140,17 @@ $(document).ready(() => {
     // Handle back button
     $("#back").click(() => {
         logger("clicked back button");
-        back();
+        //back();
         // TODO check if works properly
-        const prev = carousel[i]; // get prev track obj
+        //const prev = carousel[i]; // get prev track obj
+        const prev = HISTORY.pop();
         if (prev) {
-            updateDom(prev.title, prev.permalink_url, prev.artist);
+            CURRENT = prev;
+            HISTORY.unshift(null);
+            updateDom(CURRENT.title, CURRENT.permalink_url, CURRENT.artist);
         } else {
             logger("NO MORE TRACKS");
+            HISTORY.unshift(null);
         }
     });
 });
