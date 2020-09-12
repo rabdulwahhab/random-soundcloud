d19b0be5ab7dc987a9f087c7a54dda7115e04159
rayyan <rayyan10@bellsouth.net>
2020-09-12 00:01:45 -0500

    🧹  CHORE: add compression on all routes

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // import express web framework
 var express = require('express');
 var app = express();
+var compression = require('compression');
 var controller = require('./controllers/controller'); // store function from module
 
 
 // setup ejs template engine
 app.set('view engine', 'ejs');
 
-// static files
+// static + middleware
 app.use(express.static(__dirname + '/public'));
+app.use(compression()); // compress data on all routes
 
 // fire controllers
 controller(app);
